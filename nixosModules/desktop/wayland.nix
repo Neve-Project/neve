d15a81cc88b3b10e21a9067c5c2308529829f317
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-11-13 11:50:25 +0100

    qt programs autoscaling

--- nixosModules/desktop/wayland.nix
+++ nixosModules/desktop/wayland.nix
@@ -26,6 +26,7 @@
         NIXOS_OZONE_WL = "1";
         GDK_BACKEND = "wayland,x11,*";
         QT_QPA_PLATFORM = "wayland;xcb";
+        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       };
     };
     # Configure xwayland
