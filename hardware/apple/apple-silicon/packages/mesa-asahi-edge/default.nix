9501d472a439c9990589a34a4cfafbacce9052ec
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-11-22 11:03:56 +0100

    asahi mesa cleanup

--- hardware/apple/apple-silicon/packages/mesa-asahi-edge/default.nix
+++ hardware/apple/apple-silicon/packages/mesa-asahi-edge/default.nix
@@ -10,7 +10,7 @@
     Xplugin = null;
   })
   .override {
-    galliumDrivers = ["swrast" "asahi" "virgl" "zink"];
+    galliumDrivers = ["swrast" "asahi"];
     vulkanDrivers = ["swrast" "asahi"];
     enableGalliumNine = false;
     # libclc and other OpenCL components are needed for geometry shader support on Apple Silicon
@@ -51,35 +51,6 @@
       # do not want to add the dependencies
       "-Dlibunwind=disabled"
       "-Dlmsensors=disabled"
-      # add options from Fedora Asahi's meson flags we're missing
-      # some of these get picked up automatically since
-      # auto-features is enabled, but better to be explicit
-      # in the same places as Fedora is explicit
-      "-Dgallium-opencl=icd"
-      "-Dgallium-rusticl=true"
-      "-Dgallium-rusticl-enable-drivers=asahi"
-      "-Degl=enabled"
-      "-Dgbm=enabled"
-      "-Dopengl=true"
-      "-Dshared-glapi=enabled"
-      "-Dgles1=enabled"
-      "-Dgles2=enabled"
-      "-Dglx=dri"
-      "-Dglvnd=enabled"
-      # enable LLVM specifically (though auto-features seems to turn it on)
-      # and enable shared-LLVM specifically like Fedora Asahi does
-      # perhaps the lack of shared-llvm was/is breaking rusticl? needs investigation
-      "-Dllvm=enabled"
-      "-Dshared-llvm=enabled"
-      # add in additional options from mesa-asahi's meson options,
-      # mostly to make explicit what was once implicit (the Nix way!)
-      "-Degl-native-platform=wayland"
-      "-Dandroid-strict=false"
-      "-Dpower8=disabled"
-      "-Dvideo-codecs=vp9dec"
-      # save time, don't build tests
-      "-Dbuild-tests=false"
-      "-Denable-glcpp-tests=false"
     ];
 
   # replace patches with ones tweaked slightly to apply to this version
